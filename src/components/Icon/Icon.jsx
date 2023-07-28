c62ecdbc104d6748ba5830a1b7f64c9a2b989cfb
ShubhamTiwari55 <stofficial059@gmail.com>
2023-07-28 22:50:44 +0530

    memo function added in icon.jsx

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,9 +1,10 @@
 import {FaTimes, FaPen, FaRegCircle} from 'react-icons/fa';
-
+import {memo} from 'react';
 function Icon({name}){
     if(name=="circle") return <FaRegCircle/>
     else if(name == "cross") return <FaTimes/>
     else return <FaPen/>
 }
 
-export default Icon;
+export default memo(Icon);
+//memo is used for optimisation so that on touching or hovering the card, icons don't get re-render//
